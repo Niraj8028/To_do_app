27091d65504c300258761dc49f736ccf1792f0f8
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-10 21:29:11 +0530

    Dispatch sent

--- src/Components/Todoform.js
+++ src/Components/Todoform.js
@@ -1,28 +1,51 @@
 import React from 'react'
 import { TodoContext } from '../Context/TodoContext'
 import { ADD_TODO } from '../Context/Action.Type'
-import {Form,FormGroup,Input,Button,InputGroup,InputGroupAddon} from "reactstrap"
+import { Form, FormGroup, Input, Button, InputGroup,InputGroupText} from "reactstrap"
+import { useContext ,useState} from 'react'
+import {v4} from "uuid";
 
+const Todoform=()=> {
+    const[todostring,setTodostring]=useState("");
+    const {dispatch}=useContext(TodoContext);
 
-function Todoform() {
-  return (
-    <div>
-        <Form>
-            <FormGroup>
-                <InputGroup>
-                    
-                    <input type="text" name="todo" id="todo" placeholder='Your next todo'/>                
-                    
-                    
-                    
-                </InputGroup>
-                <Button color='success'>
-                    Add
-                    </Button>
-            </FormGroup>
-        </Form>
-    </div>
-  )
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        if(todostring===""){
+            return alert("please enter todo")
+        }
+        const todo={
+            todostring,
+            id: v4()
+        }
+        dispatch({
+            type:ADD_TODO,
+            payload:todo
+        })
+        setTodostring("");
+    }
+    return (
+        <div>
+            <Form onSubmit={handleSubmit}>
+                <FormGroup>
+                    <InputGroup>
+                        <InputGroupText>
+                        <Input type="text" 
+                            name="todo" 
+                            id="todo" 
+                            placeholder='Your next todo'
+                            value={todostring}
+                            onChange={e=>setTodostring(e.target.value)}/> 
+                            <Button color='success'>
+                        Add 
+                        </Button>
+                            </InputGroupText> 
+                    </InputGroup>
+                        
+                </FormGroup>
+            </Form>
+        </div>
+    )
 }
 
 export default Todoform
